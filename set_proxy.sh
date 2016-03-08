897069b1a3ac3eb5aa1d8886dcf0f8700544730f
Manuel Sanchez del Rio <srio@esrf.eu>
2016-03-08 16:16:22 +0100

    added file

---
+++ set_proxy.sh
@@ -0,0 +1,8 @@
+export NO_PROXY=localhost,127.0.0.0/8,*.esrf.fr,*.ill.fr
+export http_proxy=http://proxy.esrf.fr:3128/
+export ftp_proxy=ftp://proxy.esrf.fr:3128/
+export all_proxy=socks://proxy.esrf.fr:3128/
+export ALL_PROXY=socks://proxy.esrf.fr:3128/
+export socks_proxy=socks://proxy.esrf.fr:3128/
+export https_proxy=https://proxy.esrf.fr:3128/
+export no_proxy=localhost,127.0.0.0/8,*.esrf.fr,*.ill.fr
