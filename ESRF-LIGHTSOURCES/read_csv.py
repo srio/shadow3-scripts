8e3342a6fd981780eba24d68ba44ee49d57e730d
Manuel Sanchez del Rio <srio@esrf.eu>
2017-05-03 15:51:43 +0200

    syned json files

--- ESRF-LIGHTSOURCES/read_csv.py
+++ ESRF-LIGHTSOURCES/read_csv.py
@@ -1,8 +1,13 @@
 
+
+
 from syned.storage_ring.electron_beam import ElectronBeam
 from syned.storage_ring.magnetic_structures.undulator import Undulator
 from syned.storage_ring.light_source import LightSource
 import numpy
+
+
+
 
 f = open('ID_data_jan_2017_srio.csv','r')
 txt = f.read().split('\n')
@@ -20,31 +25,39 @@ for i,line in enumerate(txt):
 
 
         src1._current = 0.2
+        src1._number_of_bunches = 1
+        src1._energy_spread = 0.001
 
         if year == "2017":
             src1._energy_in_GeV = 6.037
 
             if numpy.mod(int(Section),2):
                 source = "LowBeta"
-                src1.set_sigmas_all(sigma_x=0.0,sigma_xp=0.0,sigma_y=0.0,sigma_yp=0.0)
                 print("Section: %s Lb"%Section)
+                src1.set_sigmas_all(37.4e-6,106.9e-6,3.5e-6,1.2e-6)
             else:
                 source = "HighBeta"
-                src1.set_sigmas_all(sigma_x=0.0,sigma_xp=0.0,sigma_y=0.0,sigma_yp=0.0)
                 print("Section: %s Hb"%Section)
+                src1.set_sigmas_all(387.8e-6,10.3e-6,3.5e-6,1.2e-6)
+
         else:
-            source = "EBS"
             src1._energy_in_GeV = 6.0
-            src1.set_sigmas_all(sigma_x=0.0,sigma_xp=0.0,sigma_y=0.0,sigma_yp=0.0)
+            source = "EBS"
             print("Section: %s EBS"%Section)
+            src1.set_sigmas_all(27.2e-6,5.2e-6,3.4e-6,1.4e-6)
 
-
+        src2._K_horizontal = float(Kxmax)
+        src2._K_vertical = float(Kzmax)
+        src2._period_length = float(Period)*1e-3
+        src2._number_of_periods = int( float(Length) / src2._period_length)
 
         UlistS = Ulist.split("/")
         print(">>>",(UlistS[2]))
         #
-        src = LightSource("ESRF_%s_ID%s_%s_%s_%s"%(source,Section,UlistS[0],UlistS[1],UlistS[2]),src1,src2)
+        name = "ESRF_ID%s_%s_%s_%s"%(Section,source,UlistS[1],UlistS[2])
+        src = LightSource(name,src1,src2)
 
         print(src.info())
+        src.to_json(name+'.json')
 
 
