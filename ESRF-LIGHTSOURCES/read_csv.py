e8bcc353cc42a06199390f135871a7d9bf573c9f
Manuel Sanchez del Rio <srio@esrf.eu>
2017-08-22 12:42:07 +0200

    updated lattice values to S28D.mat

--- ESRF-LIGHTSOURCES/read_csv.py
+++ ESRF-LIGHTSOURCES/read_csv.py
@@ -26,25 +26,30 @@ for i,line in enumerate(txt):
 
         src1._current = 0.2
         src1._number_of_bunches = 1
-        src1._energy_spread = 0.001
 
         if year == "2017":
+            src1._energy_spread = 0.0011
             src1._energy_in_GeV = 6.037
 
             if numpy.mod(int(Section),2):
                 source = "LowBeta"
                 print("Section: %s Lb"%Section)
-                src1.set_sigmas_all(37.4e-6,106.9e-6,3.5e-6,1.2e-6)
+                # src1.set_sigmas_all(37.4e-6,106.9e-6,3.5e-6,1.2e-6)
+                src1.set_sigmas_all(5.03973e-05,0.000107192,3.44459e-06,1.16124e-06)
             else:
                 source = "HighBeta"
                 print("Section: %s Hb"%Section)
-                src1.set_sigmas_all(387.8e-6,10.3e-6,3.5e-6,1.2e-6)
+                # src1.set_sigmas_all(387.8e-6,10.3e-6,3.5e-6,1.2e-6)
+                src1.set_sigmas_all(0.000414971,1.03149e-05,3.43353e-06,1.16498e-06)
 
         else:
+            src1._energy_spread = 9.3339e-04
+            # src1._energy_spread = 9e-04
             src1._energy_in_GeV = 6.0
             source = "EBS"
             print("Section: %s EBS"%Section)
-            src1.set_sigmas_all(27.2e-6,5.2e-6,3.4e-6,1.4e-6)
+            # src1.set_sigmas_all(27.2e-6,5.2e-6,3.4e-6,1.4e-6)
+            src1.set_sigmas_all(3.01836e-05,4.36821e-06,3.63641e-06,1.37498e-06)
 
         src2._K_horizontal = float(Kxmax)
         src2._K_vertical = float(Kzmax)
